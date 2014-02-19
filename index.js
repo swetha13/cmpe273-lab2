9cf260c263ce47feed1ce6ce39e36012b1fa7fca
Swetha <swetha.patnala@gmail.com>
2014-02-19 12:08:05 -0800

    Added implementations for PUT,POST,DELETE
    Added as per Lab2 assignment

--- index.js
+++ index.js
@@ -12,51 +12,88 @@ app.use('/', main);
 
 function main(request, response, next) {
 	switch (request.method) {
-		case 'GET': get(request, response); break;
-		case 'POST': post(request, response); break;
-		case 'DELETE': del(request, response); break;
-		case 'PUT': post(request, response); break;
+                case 'GET': get(request, response); break;
+                case 'POST': post(request, response); break;
+                case 'DELETE': del(request, response); break;
+                case 'PUT': put(request, response); break;
 	}
 };
 
 function get(request, response) {
-	var cookies = request.cookies;
-	console.log(cookies);
-	if ('session_id' in cookies) {
-		var sid = cookies['session_id'];
-		if ( login.isLoggedIn(sid) ) {
-			response.setHeader('Set-Cookie', 'session_id=' + sid);
-			response.end(login.hello(sid));	
-		} else {
-			response.end("Invalid session_id! Please login again\n");
-		}
-	} else {
-		response.end("Please login via HTTP POST\n");
-	}
+        var cookies = request.cookies;
+        console.log(cookies);
+        if ('session_id' in cookies) {
+                var sid = cookies['session_id'];
+                if ( login.isLoggedIn(sid) ) {
+                        response.setHeader('Set-Cookie', 'session_id=' + sid);
+                        response.end(login.hello(sid));
+                } else {
+                        response.end("Invalid session_id! Please login again\n");
+                }
+        } else {
+                response.end("Please login via HTTP POST\n");
+        }
 };
 
 function post(request, response) {
-	// TODO: read 'name and email from the request.body'
-	// var newSessionId = login.login('xxx', 'xxx@gmail.com');
-	// TODO: set new session id to the 'session_id' cookie in the response
-	// replace "Logged In" response with response.end(login.hello(newSessionId));
 
-	response.end("Logged In\n");
+     var name = request.body.name;
+     var email = request.body.email;
+     var newSessionId = login.login(name, email);
+     response.setHeader('Set-Cookie', 'session_id=' + newSessionId);
+     response.end(login.hello(newSessionId));
+     console.log(newSessionId);
+
 };
 
 function del(request, response) {
-	console.log("DELETE:: Logout from the server");
- 	// TODO: remove session id via login.logout(xxx)
- 	// No need to set session id in the response cookies since you just logged out!
+        console.log("DELETE:: Logout from the server");
 
-  	response.end('Logged out from the server\n');
+    var cookies = request.cookies;
+        console.log(cookies);
+        if ('session_id' in cookies) {
+                var sid = cookies['session_id'];
+                if ( login.isLoggedIn(sid) ) {
+                  login.logout(sid);
+
+                response.end('Logged out from the server\n');
+                }
+                else {
+                response.end('Invalid session_id or session id already deleted\n');
+                }
+        }
+
+        else{
+                response.end('Invalid');
+        }
+
 };
 
-function put(request, response) {
-	console.log("PUT:: Re-generate new seesion_id for the same user");
-	// TODO: refresh session id; similar to the post() function
 
-	response.end("Re-freshed session id\n");
+function put(request, response) {
+        console.log("PUT:: Re-generate new seesion_id for the same user");
+        // TODO: refresh session id; similar to the post() function
+        var cookies = request.cookies;
+        console.log(cookies);
+    if ('session_id' in cookies) {
+            var sid = cookies['session_id'];
+            if ( login.isLoggedIn(sid) ) {
+
+                console.log(login.hello(sid));
+                var name1 = login.name(sid);
+                var email1 = login.email(sid);
+                login.logout(sid);
+                var refreshedSessionId = login.login(name1, email1);
+                console.log(refreshedSessionId);
+                response.end('Re-freshed session id\n');
+            }
+
+
+           else{
+                response.end('Invalid session_id or session id already deleted\n');
+           }
+
+   }
 };
 
 app.listen(8000);
